e4233a44f09ab914a05f9d21740c44063f99b3ad
SECK Ibrahima <seck11201@gmail.com>
2025-01-22 23:29:15 +0100

    Configuration du projet

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,46 +1,9 @@
-module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2021: true
-  },
-  settings: {
-    "import/resolver": {
-      "node": {
-        "extensions": [
-          ".js",
-          ".jsx"
-        ]
-      }
-    }
-  },
-  plugins: [
-    'import',
-  ],
-  extends: [
-    'eslint:recommended',
-    'plugin:import/errors',
-    'plugin:import/warnings',
-  ],
-  overrides: [
-    {
-      env: {
-        node: true
-      },
-      files: [
-        '.eslintrc.{js,cjs}'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      }
-    }
-  ],
-  parserOptions: {
-    ecmaVersion: 'latest',
-    sourceType: 'module'
-  },
-  rules: {
-    "no-unused-vars" : "warn",
-"no-empty" : "warn",
-  }
-}
+import globals from "globals";
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {languageOptions: { globals: globals.browser }},
+  pluginJs.configs.recommended,
+];
---
+++ src/index.js
@@ -0,0 +1,32 @@
+import { Engine, FreeCamera, HemisphericLight, MeshBuilder, Scene, Vector3 } from "@babylonjs/core";
+
+window.onload = () => {
+    console.log('Hello World!');
+
+    const canvas = document.getElementById("renderCanvas");
+    let engine = new Engine(canvas, true);
+
+    const createScene = function () {
+        const scene = new Scene(engine);
+        const camera = new FreeCamera("camera1", 
+            new Vector3(0, 5, -10), scene);
+        camera.setTarget(Vector3.Zero());
+        camera.attachControl(canvas, true);
+        const light = new HemisphericLight("light", 
+            new Vector3(0, 1, 0), scene);
+        light.intensity = 0.7;
+        const sphere = MeshBuilder.CreateSphere("sphere", 
+            {diameter: 2, segments: 32}, scene);
+        sphere.position.y = 1;
+        const ground = MeshBuilder.CreateGround("ground", 
+            {width: 6, height: 6}, scene);
+        return scene;
+    };
+    const scene = createScene(); 
+    engine.runRenderLoop(function () {
+            scene.render();
+    });
+    window.addEventListener("resize", function () {
+            engine.resize();
+    });
+};
