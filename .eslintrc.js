d0e90a084e84601fc2bb8f455fc0e6732a433ad5
SECK Ibrahima <seck11201@gmail.com>
2025-01-31 09:12:34 +0100

    Motification de la branche main avec installation des modules indispensable au fonctionnement du projet

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,48 +1,46 @@
-
-// eslint-disable-next-line no-undef
 module.exports = {
-    root: true,
-    env: {
-      browser: true,
-      es2021: true
-    },
-    settings: {
-      "import/resolver": {
-        "node": {
-          "extensions": [
-            ".js",
-            ".jsx"
-          ]
-        }
+  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
+  settings: {
+    "import/resolver": {
+      "node": {
+        "extensions": [
+          ".js",
+          ".jsx"
+        ]
       }
-    },
-    plugins: [
-      'import',
-    ],
-    extends: [
-      'eslint:recommended',
-      'plugin:import/errors',
-      'plugin:import/warnings',
-    ],
-    overrides: [
-      {
-        env: {
-          node: true
-        },
-        files: [
-          '.eslintrc.{js,cjs}'
-        ],
-        parserOptions: {
-          sourceType: 'script'
-        }
+    }
+  },
+  plugins: [
+    'import',
+  ],
+  extends: [
+    'eslint:recommended',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+  ],
+  overrides: [
+    {
+      env: {
+        node: true
+      },
+      files: [
+        '.eslintrc.{js,cjs}'
+      ],
+      parserOptions: {
+        sourceType: 'script'
       }
-    ],
-    parserOptions: {
-      ecmaVersion: 'latest',
-      sourceType: 'module'
-    },
-    rules: {
-      "no-unused-vars" : "warn",
-  "no-empty" : "warn",
+    }
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  rules: {
+    "no-unused-vars" : "warn",
+    "no-empty" : "warn",
   }
 }
